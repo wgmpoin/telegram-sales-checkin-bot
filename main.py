753bf879520e29cdf254e7cc1dadf00bb5460dc5
wgmpoin <ptbis.pm1@gmail.com>
2025-06-09 13:39:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Konfigurasi logging untuk memantau aktivitas bot
+# Konfigurasi logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-# Ganti dengan token bot Telegram kamu
-BOT_TOKEN = '7973184485:AAHSRCxhazoqMm0TTOQ6ZNoiiCYe6UHkGeQ'
+# Token bot Telegram kamu
+BOT_TOKEN = '7973184485:AAFe_vtRK4nYjf_Re3J4HSSaeF07dM2HyoM'
 
-# Fungsi saat pengguna kirim /start
+# Fungsi saat /start dikirim
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_full_name = update.effective_user.full_name
     await update.message.reply_text(f"Halo {user_full_name}, selamat datang!")
 
-# Fungsi utama untuk menjalankan bot
+# Fungsi utama
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
-# Menjalankan fungsi utama saat file dieksekusi
 if __name__ == '__main__':
     main()
